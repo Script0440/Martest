7cda206026ba75a7c89890db01219ad4e49d37f0
Script <110091553+Script0440@users.noreply.github.com>
2024-03-22 21:44:10 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const menu = document.getElementById("#burgerMenu");
 const btn = document.getElementById("#burgerButton");
 const links = document.querySelectorAll(".nav-link");
 const catalogInfo = document.getElementById("catalogInfo");
-
+const header = document.getElementById("header");
 const catalogButtonLevel = document.getElementById("levelSelectButton");
 const catalogButtonBranch = document.getElementById("branchSelectButton");
 
@@ -11,7 +11,15 @@ btn.addEventListener("click", openBurger);
 function openBurger(e) {
   e.preventDefault();
   menu.classList.toggle("active");
+  header.style.borderBottom = "1px solid #fff";
 }
+window.addEventListener("scroll", () => {
+  header.style.borderBottom = "none";
+  if (window.scrollY >= 100 || menu.classList.contains("active")) {
+    header.style.borderBottom = "1px solid #fff";
+    return;
+  }
+});
 
 links.forEach((item) =>
   item.addEventListener("click", () => {
